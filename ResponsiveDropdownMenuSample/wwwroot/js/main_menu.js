c64f194b61bda7e4829f20a06d1f74ef2b385db3
LuongAnh <luonganh@gmail.com>
2024-01-22 17:24:26 +0700

    Add project files.

---
+++ ResponsiveDropdownMenuSample/wwwroot/js/main_menu.js
@@ -0,0 +1,19 @@
+﻿/** code by webdevtrick ( https://webdevtrick.com ) **/
+(function ($) {
+    $(function () {
+        $('.main-menu ul li a:not(:only-child)').click(function (e) {
+            $(this).siblings('.nav-dropdown').toggle();
+            $('.dropdown').not($(this).siblings()).hide();
+            e.stopPropagation();
+        });
+        $('html').click(function () {
+            $('.nav-dropdown').hide();
+        });
+        $('#nav-toggle').click(function () {
+            $('.main-menu ul').slideToggle();
+        });
+        $('#nav-toggle').on('click', function () {
+            this.classList.toggle('active');
+        });
+    });
+})(jQuery);
